510a519f6a936da5d32478af72f6faa2b34a53c2
Shukridozom <Shukridozom@hotmail.com>
2023-10-26 22:56:28 +0300

    Implementing an API to use the notification service

---
+++ Notification-Service/Controllers/FCMController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notification_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FCMController : ControllerBase
+    {
+        private readonly IFCM_Service fcm;
+
+        public FCMController(IFCM_Service fcm)
+        {
+            this.fcm = fcm;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendNotification(NotificationDto notification)
+        {
+            try
+            {
+                await fcm.SendNotification(notification.Token, notification.Title, notification.Body);
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("Unvalid FCM Token");
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Notification-Service/NotificationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notification_Service
+{
+    public class NotificationDto
+    {
+        [Required]
+        public string Token { get; set; }
+        [MaxLength(100)]
+        public string? Title { get; set; }
+        [MaxLength(900)]
+        public string? Body { get; set; }
+    }
+}
